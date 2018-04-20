6dba687f57ae402f8578cface235842c342c021d
khurramAkram30 <khurram.akram43@gmail.com>
2018-04-21 01:16:28 +0500

    mvc practice

---
+++ WebApplication1/Controllers/passingDataController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+
+namespace WebApplication1.Controllers
+{
+    public class passingDataController : Controller
+    {
+        // GET: passingData
+        public ActionResult Index()
+        {
+            List<product> Products = new List<product>();
+
+            Products.Add(new product() { id = 1, name = "burger", price = 350, image = "1.png" });
+
+            Products.Add(new product() { id = 2, name = "burgerking", price = 400, image = "1.png" });
+
+            ViewBag.products = Products;
+
+            ViewData["prod"] = Products;
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class product
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public float price{ get; set; }
+
+
+        public string image { get; set; }
+        }
+}
